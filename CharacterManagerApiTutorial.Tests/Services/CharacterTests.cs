490a8273045a7a2cc53d1757cb0bb39fabd45fc5
Zendrakk <ccnieman@gmail.com>
2025-08-15 20:49:55 -0600

    Updated test again

--- CharacterManagerApiTutorial.Tests/Services/CharacterTests.cs
+++ CharacterManagerApiTutorial.Tests/Services/CharacterTests.cs
@@ -57,8 +57,8 @@ namespace CharacterManagerApiTutorial.Tests.Services
             Assert.NotNull(result.Value);
             Assert.IsType<Result<List<CharacterDto>>>(result);
             Assert.Equal(2, result.Value.Count);
-            Assert.Contains(result.Value, c => c.Name == "tester");
-            Assert.Contains(result.Value, c => c.Name == "tester2");
+            Assert.Contains(result.Value, c => c.Name == "tester" && c.RealmName == "Frostgard");
+            Assert.Contains(result.Value, c => c.Name == "tester2" && c.RealmName == "Frostgard");
 
             mockLogger.Verify(x =>
                 x.Log(
